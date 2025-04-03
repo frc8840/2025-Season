fe269d5c6bc471ce8f9695346405c4d57c6b1f5b
Evan <kuoevan145@gmail.com>
2025-04-02 17:01:18 -0700

    added canScore function

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -2,7 +2,9 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.apriltag.*;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.wpilibj.Filesystem;
@@ -58,6 +60,69 @@ public class Vision extends SubsystemBase {
     }
   }
 
+  public boolean canScore(Boolean isLeft) {
+    List<PhotonPipelineResult> result = photonCamera.getAllUnreadResults();
+    PhotonPipelineResult lastResult = result.get(result.size() - 1);
+    if (!lastResult.hasTargets()) {
+      return false;
+    }
+
+    PhotonTrackedTarget target = lastResult.getBestTarget();
+    int tagId = target.getFiducialId();
+
+    List<Integer> noScoreTags = List.of(1, 2, 3, 4, 5, 12, 13, 14, 15, 16);
+
+    if (noScoreTags.contains(tagId)) {
+      return false;
+    }
+
+    Optional<Pose3d> tagPoseOptional = fieldLayout.getTagPose(tagId);
+    if (tagPoseOptional.isEmpty()) {
+      return false;
+    }
+
+    Optional<EstimatedRobotPose> poseEstimate = photonPoseEstimator.update(lastResult);
+    if (poseEstimate.isEmpty()) {
+      return false;
+    }
+
+    Pose2d robotPose = poseEstimate.get().estimatedPose.toPose2d();
+    Pose2d tagPose = tagPoseOptional.get().toPose2d();
+
+    Transform2d robotToTag = new Transform2d(robotPose, tagPose);
+
+    double xOffset = robotToTag.getX();
+    double yOffset = robotToTag.getY();
+    double angleOffset = robotToTag.getRotation().getDegrees();
+
+    // Parameters - you can tune these
+    double distanceThreshold = 1.5; // meters
+    double yThresholdLeft = 0.5; // meters
+    double yThresholdRight = -0.5; // meters
+    double angleThreshold = 25.0; // degrees
+
+    boolean canScoreLeft =
+        xOffset < distanceThreshold
+            && xOffset > 0
+            && yOffset < yThresholdLeft
+            && angleOffset < angleThreshold;
+    boolean canScoreRight =
+        xOffset < distanceThreshold
+            && xOffset > 0
+            && yOffset > yThresholdRight
+            && angleOffset < angleThreshold;
+
+    // Optional telemetry
+    SmartDashboard.putNumber("Score Distance", xOffset);
+    SmartDashboard.putNumber("Score Angle", angleOffset);
+
+    if (isLeft) {
+      return canScoreLeft;
+    } else {
+      return canScoreRight;
+    }
+  }
+
   @Override
   public void periodic() {
 
@@ -80,7 +145,8 @@ public class Vision extends SubsystemBase {
     Logger.LogPeriodic("yaw: " + target.getYaw());
     Logger.LogPeriodic("pitch: " + target.getPitch());
     Logger.LogPeriodic("area: " + target.getArea());
-    // Logger.LogPeriodic("Tag Pose from layout: " + fieldLayout.getTagPose(target.getFiducialId()));
+    // Logger.LogPeriodic("Tag Pose from layout: " +
+    // fieldLayout.getTagPose(target.getFiducialId()));
     Logger.LogPeriodic("Best Transform: " + target.getBestCameraToTarget());
     Logger.LogPeriodic("Alt  Transform: " + target.getAlternateCameraToTarget());
 
@@ -108,6 +174,8 @@ public class Vision extends SubsystemBase {
           // To update to the pose we calculate with this
           // swerve.resetOdometry(pose2d);
         });
-    SmartDashboard.putNumber("End of vision loop", 1);
+    // SmartDashboard.putNumber("End of vision loop", 1);
+    SmartDashboard.putBoolean("Can Score Left", canScore(true));
+    SmartDashboard.putBoolean("Can Score Right", canScore(false));
   }
 }
